20bf9d570c056ee56c355db156a5daa079f53e24
Sondre071 <sn@dokflyt.no>
2024-01-23 10:22:08 +0100

    Sorteringsfunksjonalitet

---
+++ src/assets/retired/Table (old).vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="item in jsonData">{{ item }}</li>
+        </ul>
+    </div>
+    <br><br>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th v-for="item in colNames" >{{ item }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td v-for="item in rowNames" >{{ item }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script setup lang="ts">
+import jsonData from "../../public/data.json";
+const colNames = Object.keys(jsonData[0]);
+const rowNames = Object.values(jsonData[0]);
+
+</script>
+
+<style scoped></style>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,4 +1,9 @@
 <template>
+    <div class="searchBar">
+        <div class="input-group-mb-5">
+            <input type="search" class="form-control" v-model="searchQuery" placeholder="Name" aria-describedby="button-addon2">
+        </div>
+    </div>
     <table id="tableComponent" class="table table-bordered table-striped">
         <thead>
             <tr>
@@ -8,57 +13,48 @@
             </tr>
         </thead>
         <tbody>
-            <tr v-for="item in jsonData" :key="jsonData.indexOf(item) + 1">
+            <tr v-for="item in json" :key="json.indexOf(item) + 1">
                 <td v-for="key in jsonKeys" :key="key"> {{ item[key as keyof typeof item] }}</td>
             </tr>
         </tbody>
     </table>
     <h1>hi</h1>
-    <h2>{{ updatedList }}</h2>
+    <h2>{{ json }}</h2>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue"
+import { ref, computed } from "vue"
 import { sortBy } from "lodash"
 import "bootstrap/dist/css/bootstrap.min.css";
 //import jsonData from "../assets/data.json"
 let json = ref([
-    {"name":"Aria","race":"Half-Elf","className":"Rogue","level":5,"createdAt":"2021-09-01"},
-    {"name":"Marcus","race":"Human","className":"Paladin","level":1,"createdAt":"2021-09-15"},
-    {"name":"Tessa","race":"Dwarf","className":"Bard","level":8,"createdAt":"2021-09-22"},
-    {"name":"Erik","race":"Dwarf","className":"Barbarian","level":3,"createdAt":"2021-10-01"},
-    {"name":"Lila","race":"Tiefling","className":"Warlock","level":7,"createdAt":"2021-10-07"},
-    {"name":"Niamh","race":"Elf","className":"Ranger","level":2,"createdAt":"2021-10-15"},
-    {"name":"Tristan","race":"Half-Orc","className":"Fighter","level":6,"createdAt":"2021-10-23"},
-    {"name":"Ava","race":"Halfling","className":"Cleric","level":4,"createdAt":"2021-11-01"}
-    ])
-let jsonData = json.value
+    { "name": "Aria", "race": "Half-Elf", "className": "Rogue", "level": 5, "createdAt": "2021-09-01" },
+    { "name": "Marcus", "race": "Human", "className": "Paladin", "level": 1, "createdAt": "2021-09-15" },
+    { "name": "Tessa", "race": "Dwarf", "className": "Bard", "level": 8, "createdAt": "2021-09-22" },
+    { "name": "Erik", "race": "Dwarf", "className": "Barbarian", "level": 3, "createdAt": "2021-10-01" },
+    { "name": "Lila", "race": "Tiefling", "className": "Warlock", "level": 7, "createdAt": "2021-10-07" },
+    { "name": "Niamh", "race": "Elf", "className": "Ranger", "level": 2, "createdAt": "2021-10-15" },
+    { "name": "Tristan", "race": "Half-Orc", "className": "Fighter", "level": 6, "createdAt": "2021-10-23" },
+    { "name": "Ava", "race": "Halfling", "className": "Cleric", "level": 4, "createdAt": "2021-11-01" }
+])
 
+let jsonKeys = Object.keys(json.value[0]);
 
-
-
-let filteredlist;
-let jsonKeys = Object.keys(jsonData[0]);
-
-// ==== functions ==== //
 
 let sort = ref(false);
-let updatedList = ref<listTypes[]>([])
 
-interface listTypes {
-    name: string;
-    race: string;
-    className: string;
-    level: number;
-    createdAt: string;
-
-}
-
-const sortTable = (key: string|number) => {
+const sortTable = (key: string | number) => {
     sort.value = true
-    updatedList.value = sortBy(jsonData, key)
-    jsonData = sortBy(jsonData, key)
+    json.value = sortBy(json.value, key)
 }
+
+const filteredList = computed(() => {
+    return sortedList.value.filter((product => {
+        return (
+            product.Name.toLowerCase().indexOf(searchQuery.value.toLowerCase()) != -1
+        )
+    }))
+})
 
 
 </script>
