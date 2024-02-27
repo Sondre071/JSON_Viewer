bb247b594469425dc648b84bda9773e7e96e88a5
Sondre <155721341+Sondre071@users.noreply.github.com>
2024-02-27 09:49:10 +0100

    Reverting main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { createPinia } from 'pinia'
+//import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).use(createPinia()).mount('#app')
+createApp(App).mount('#app')
